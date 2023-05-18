5ac77237d97800301b431fee2159a7c5baa2bb3b
Jishnu P Dileep <jishnu@applab.qa>
2023-05-17 18:20:15 -0700

    range function completed

--- app/src/main/java/com/stark/customhorizontalcalender/MainActivity.kt
+++ app/src/main/java/com/stark/customhorizontalcalender/MainActivity.kt
@@ -131,30 +131,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun RecyclerView.onScrollDoneGetPosition(onScrollUpdate: (Int) -> Unit) {
-        this.addOnScrollListener(object :
-            RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-            }
 
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                when (newState) {
-                    AbsListView.OnScrollListener.SCROLL_STATE_FLING -> {
-                    }
-                    AbsListView.OnScrollListener.SCROLL_STATE_IDLE -> {
-                        print("When User Done it's Scroll")
-                        val currentPosition =
-                            (this@onScrollDoneGetPosition.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
-                        onScrollUpdate.invoke(currentPosition)
-                    }
-                    AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL -> {
-                    }
-                }
-            }
-        })
-    }
 
     private fun transitionManagerForRecyclerview(){
         val changeBounds = ChangeBounds()
@@ -319,9 +296,7 @@ class MainActivity : AppCompatActivity() {
         return try {
             val format = SimpleDateFormat("dd-MM-yyyy")
             val currentDate =  format.format(date.time)
-            val isDateEnabledStatus = selectedDates.contains(currentDate)
-            Log.e("isDateEnabledStatus","<<< $isDateEnabledStatus")
-            isDateEnabledStatus
+            selectedDates.contains(currentDate)
         }catch (e:Exception){
             Log.e("exception","${e.message}")
             false
--- app/src/main/java/com/stark/customhorizontalcalender/utlis/EtxnUtils.kt
+++ app/src/main/java/com/stark/customhorizontalcalender/utlis/EtxnUtils.kt
@@ -2,6 +2,9 @@ package com.stark.customhorizontalcalender.utlis
 
 import android.content.res.Resources
 import android.view.View
+import android.widget.AbsListView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
@@ -49,5 +52,30 @@ fun View.hide(){
 
 fun View.invisible(){
     this.visibility = View.INVISIBLE
+}
+
+fun RecyclerView.onScrollDoneGetPosition(onScrollUpdate: (Int) -> Unit) {
+    this.addOnScrollListener(object :
+        RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            when (newState) {
+                AbsListView.OnScrollListener.SCROLL_STATE_FLING -> {
+                }
+                AbsListView.OnScrollListener.SCROLL_STATE_IDLE -> {
+                    print("When User Done it's Scroll")
+                    val currentPosition =
+                        (this@onScrollDoneGetPosition.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                    onScrollUpdate.invoke(currentPosition)
+                }
+                AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL -> {
+                }
+            }
+        }
+    })
 }
 
